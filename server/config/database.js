ddc821ff8e8b2a697b62fec28122be16780dbcba
nandinishiny <nandinishiny136@gmail.com>
2023-08-23 13:50:40 +0530

    adding few more

--- server/config/database.js
+++ server/config/database.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 export const mongoConnect = async()=>{
     await mongoose.connect(process.env.DB_URI,{
-        dbName:"ecomtwo",
+        dbName:"nandyecom",
         useNewUrlParser: true,
          useUnifiedTopology: true
     }).then((data)=>console.log(`db is connected and the host is ${data.connection.host}`))
