3c8a2b155d046ef539f0cdf4850d5f35370dd294
nandinishiny <nandinishiny136@gmail.com>
2023-09-06 17:11:19 +0530

    before server deployment

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -1,20 +1,8 @@
 import cloudinaryM from 'cloudinary'
 import dotenv from 'dotenv';
 dotenv.config({path:"server/config/config.env"})
-
-
 const cloudinary= cloudinaryM.v2;
-// try {
-//   cloudinary.config({
-//     cloud_name: 'ecomandco',
-//     api_key: '165247421741665',
-//     api_secret: 'uFmId5vAat-D1BweQgCb7QvOxzE'
-//   });
-  
-// } catch (error) {
-//   console.log(error)
-  
-// }
+
 try {
   cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
